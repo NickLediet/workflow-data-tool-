176e1345ca2d469d72191165cb9be333a704a7bc
Nick Lediet <nick@tagsforhope.com>
2019-04-29 16:51:40 -0400

    update .env path

---
+++ post-commit.sh
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env bash
+
+. .env
+
+API_KEY=$RESCUETIME_API_KEY
+
+# REQUIRED FIELDS - Today's date and commit message
+
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+DATE_TODAY=$(date +"%Y-%m-%d")
+
+# You can edit the LABEL value if you would rather
+# describe these commits differently.
+
+LABEL='Commit'
+
+# See more filtering examples in README.md
+
+# if [[ ${#MESSAGE} -gt 16 ]]; then
+  curl --data "key=$API_KEY&highlight_date=$DATE_TODAY&description=$MESSAGE&source=$LABEL" https://www.rescuetime.com/anapi/highlights_post
+# fi
